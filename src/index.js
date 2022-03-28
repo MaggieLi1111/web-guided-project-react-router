4ee5216e584d351188cbebd1b3c636460bd2ed10
Casey Harding <casey.r.harding@gmail.com>
2022-03-28 12:37:55 -0400

    Through to Link / Route

--- src/index.js
+++ src/index.js
@@ -8,9 +8,18 @@ import App from './components/App'
 import './styles.less'
 
 // 👉 STEP 1 - Import React Router's Router
+import { BrowserRouter as Router } from "react-router-dom";
 
 render(
   // Wrap the <App /> in a provider
-  <App />
+  <Router>
+    <App />
+  </Router>
   , document.querySelector('#root')
 )
+
+/**
+ * history -> about where you've been
+ * location -> about where you are
+ * match -> query parameters
+ */
